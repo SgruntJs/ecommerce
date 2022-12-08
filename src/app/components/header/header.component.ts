e17c6803464006d659cf0e5046bc4572df766e9c
annalisape <anna.moz.pe@gmail.com>
2022-12-08 11:55:18 +0100

    update focus on select

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
-import { of } from 'rxjs';
-import { Category } from 'src/app/models/category';
+import { Component } from "@angular/core";
+import { of } from "rxjs";
+import { Category } from "src/app/models/category";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
   selectedValue!: string;
   selectedCar!: string;
-
+  selectFocusClass = false;
 
   categories: Category[] = [
-    {value: 'alexa-skill', viewValue: 'Alexa Skill'},
-    {value: 'saab', viewValue: 'Alimentari e cura della casa'},
-    {value: 'amazon-warehouse', viewValue: 'Amazon Warehouse'},
-    {value: 'film-tv', viewValue: 'Film e TV'},
+    { value: "alexa-skill", viewValue: "Alexa Skill" },
+    { value: "saab", viewValue: "Alimentari e cura della casa" },
+    { value: "amazon-warehouse", viewValue: "Amazon Warehouse" },
+    { value: "film-tv", viewValue: "Film e TV" },
+    { value: "auto-moto", viewValue: "Auto e Moto - Parti e Accessori" },
   ];
 
   categories$ = of(this.categories);
