7e1195dbd18539ea7704110e544fc36a4811e487
annalisape <anna.moz.pe@gmail.com>
2022-12-09 10:45:57 +0100

    wip carousl add nex and prev event slides

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -14,4 +14,16 @@ export class CarouselComponent {
   getCurrentSlideUrl(): string {
     return `url('${this.slides[this.currentIndex].url}')`;
   }
+
+  goToNext():void {
+    const isLastSlide = this.currentIndex === this.slides.length -1;
+    const newIndex = isLastSlide ? 0 : this.currentIndex +1;
+    this.currentIndex = newIndex;
+  }
+
+  goToPrevious():void {
+    const isFirstSlide = this.currentIndex === 0;
+    const newIndex = isFirstSlide ? this.slides.length - 1 : this.currentIndex - 1;
+    this.currentIndex = newIndex;
+  }
 }
