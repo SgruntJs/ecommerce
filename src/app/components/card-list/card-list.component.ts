377cbdfe92611982b2b1f5066b8cc1b290bd9b0c
annalisape <anna.moz.pe@gmail.com>
2022-12-12 09:41:26 +0100

    positon top main container in home page

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -19,11 +19,25 @@ export class CardListComponent {
     },
     {
       id: "2",
-      thumb: "/assets/images/card2.jpg", 
+      thumb: "https://images-eu.ssl-images-amazon.com/images/G/29/GiftCards/Consumer/multi-product/HOL22/Christmas22_Card_GC_379x304_1x._SY304_CB620002760_.png", 
       title: "Offerte Lampo",
       seeMore: "Scopri di più",
       type: "deal"
     },
+    {
+      id: "3",
+      thumb: "/assets/images/card2.jpg", 
+      title: "Giochi",
+      seeMore: "Scopri di più",
+      type: "deal"
+    },
+    {
+      id: "4",
+      thumb: "https://images-eu.ssl-images-amazon.com/images/G/29/ASBA/SIC_Flexible_Desktop__Cat_Card_Lifestyle_ES_Pradorey_51x._SY304_CB610884861_.jpg", 
+      title: "Aiutiamo le imprese a crescere",
+      seeMore: "Scopri di più",
+      type: "deal"
+    }
   ];
 
 
