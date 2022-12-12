cc22ce0b619c1bdcd9b5198a7f215a556c6816fc
annalisape <anna.moz.pe@gmail.com>
2022-12-12 10:56:43 +0100

    fix difefrents height image in home

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -15,56 +15,56 @@ export class CardListComponent {
       thumb: "/assets/images/card1.jpg", 
       title: "Buoni Regalo di Natale",
       seeMore: "Scopri di più",
-      type: "deal"
+      type: "default"
     },
     {
       id: "2",
       thumb: "https://images-eu.ssl-images-amazon.com/images/G/29/GiftCards/Consumer/multi-product/HOL22/Christmas22_Card_GC_379x304_1x._SY304_CB620002760_.png", 
       title: "Offerte Lampo",
       seeMore: "Scopri di più",
-      type: "deal"
+      type: "default"
     },
     {
       id: "3",
       thumb: "/assets/images/card2.jpg", 
       title: "Giochi",
       seeMore: "Scopri di più",
-      type: "deal"
+      type: "default"
     },
     {
       id: "4",
       thumb: "https://images-eu.ssl-images-amazon.com/images/G/29/ASBA/SIC_Flexible_Desktop__Cat_Card_Lifestyle_ES_Pradorey_51x._SY304_CB610884861_.jpg", 
       title: "Aiutiamo le imprese a crescere",
       seeMore: "Scopri di più",
-      type: "deal"
+      type: "default"
     },
     {
       id: "5",
       thumb: "https://images-eu.ssl-images-amazon.com/images/G/29/Events/2022/Xmas/Homepage/XCM_CUTTLE_1488286_2673650_758x608_2X_it_IT._SY608_CB607948390_.jpg", 
       title: "Regali di Natale per tutti",
       seeMore: "Scopri il Negozio di Natale",
-      type: "deal"
+      type: "default"
     },
     {
       id: "6",
       thumb: "https://images-eu.ssl-images-amazon.com/images/G/29/Events/2022/Xmas/Homepage/XCM_CUTTLE_1488286_2673650_758x608_2X_it_IT._SY608_CB607948390_.jpg", 
       title: "Regali di Natale per tutti",
       seeMore: "Scopri il Negozio di Natale",
-      type: "deal"
+      type: "default"
     },
     {
       id: "7",
       thumb: "https://images-eu.ssl-images-amazon.com/images/G/29/Renewed/Desktop_758x608_2X._SY608_CB605045903_.jpg", 
       title: "Prodotti ricondizionati : meglio per il pianeta",
       seeMore: "Scopri il Negozio di Natale",
-      type: "deal"
+      type: "default"
     },
     {
       id: "8",
       thumb: "https://images-eu.ssl-images-amazon.com/images/G/29/Events/2022/Xmas/Homepage/XCM_CUTTLE_1488286_2673650_758x608_2X_it_IT._SY608_CB607948390_.jpg", 
       title: "Regali di Natale per tutti",
       seeMore: "Scopri il Negozio di Natale",
-      type: "deal"
+      type: "default"
     },
   ];
 
