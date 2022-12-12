e21ef3725349ed8765241063f7b771c933e20368
annalisape <anna.moz.pe@gmail.com>
2022-12-12 17:44:56 +0100

    wip menu second level

--- src/app/components/side-nav-content/side-nav-content.component.ts
+++ src/app/components/side-nav-content/side-nav-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import {Component, Renderer2, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Observable, of } from "rxjs";
 import { Sidenav } from "src/app/models/sidenav";
 
@@ -8,6 +8,9 @@ import { Sidenav } from "src/app/models/sidenav";
   styleUrls: ["./side-nav-content.component.scss"],
 })
 export class SideNavContentComponent {
+
+  @ViewChild('firstLevel') firstLevel!: ElementRef;
+  @ViewChild('secondLevel') secondLevel!: ElementRef;
   sidenav$: Observable<Sidenav[]>;
   sidenavMenu: Sidenav[] = [
     {
@@ -93,7 +96,17 @@ export class SideNavContentComponent {
       isSeparator: false,
     },
   ];
-  constructor() {
+  constructor(private renderer:Renderer2) {
     this.sidenav$ = of(this.sidenavMenu);
+  }
+
+  openSecondLevel() {
+      const first = this.firstLevel.nativeElement;
+      const second = this.secondLevel.nativeElement;
+      this.renderer.addClass(first,'menu-translateX-left');
+      this.renderer.removeClass(first,'menu-visible');
+      this.renderer.addClass(second,'menu-translateX');
+      this.renderer.addClass(second,'menu-visible');
+      this.renderer.removeClass(second,'menu-translateX-right');
   }
 }
