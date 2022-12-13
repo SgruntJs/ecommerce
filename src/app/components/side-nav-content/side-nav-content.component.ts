f54e14b25ae9395e9e0d3a6e3a8ec956529a2eb4
annalisape <anna.moz.pe@gmail.com>
2022-12-13 09:54:41 +0100

    export mock data in external file

--- src/app/components/side-nav-content/side-nav-content.component.ts
+++ src/app/components/side-nav-content/side-nav-content.component.ts
@@ -1,6 +1,7 @@
 import {Component, Renderer2, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Observable, of } from "rxjs";
 import { Sidenav } from "src/app/models/sidenav";
+import  { SIDENAV } from './sidenav';
 
 @Component({
   selector: "app-side-nav-content",
@@ -12,90 +13,8 @@ export class SideNavContentComponent {
   @ViewChild('firstLevel') firstLevel!: ElementRef;
   @ViewChild('secondLevel') secondLevel!: ElementRef;
   sidenav$: Observable<Sidenav[]>;
-  sidenavMenu: Sidenav[] = [
-    {
-      id: "1",
-      text: "di tendenza",
-      subMenu: [],
-      isHighlight: true,
-      isSeparator: false,
-    },
-    {
-      id: "2",
-      text: "Besteseller",
-      subMenu: [],
-      isHighlight: false,
-      isSeparator: false,
-    },
-    {
-      id: "3",
-      text: "Novità",
-      subMenu: [],
-      isHighlight: false,
-      isSeparator: false,
-    },
-    {
-      id: "4",
-      text: "I prodotti del momento",
-      subMenu: [],
-      isHighlight: false,
-      isSeparator: false,
-    },
-    {
-      id: "5",
-      text: "",
-      subMenu: [],
-      isHighlight: false,
-      isSeparator: true,
-    },
-    {
-      id: "6",
-      text: "Dispositivi E Contenuti Digitali",
-      subMenu: [],
-      isHighlight: true,
-      isSeparator: false,
-    },
-    {
-      id: "6",
-      text: "Amazon Prime Video",
-      subMenu: [
-        {
-          text: "Catalogo",
-          value: "1",
-        },
-        {
-          text: "Film",
-          value: "2",
-        },
-        {
-          text: "SerieTV",
-          value: "3",
-        },
-      ],
-      isHighlight: false,
-      isSeparator: false,
-    },
-    {
-      id: "7",
-      text: "Amazon Music",
-      subMenu: [
-        {
-          text: "Riproduci In Streaming",
-          value: "1",
-        },
-        {
-          text: "Amazon Music Unlimited",
-          value: "2",
-        },
-        {
-          text: "Amazon Music Prime",
-          value: "3",
-        },
-      ],
-      isHighlight: false,
-      isSeparator: false,
-    },
-  ];
+  sidenavMenu: Sidenav[] = SIDENAV;
+
   constructor(private renderer:Renderer2) {
     this.sidenav$ = of(this.sidenavMenu);
   }
