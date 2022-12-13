a4e9593a651f77eb6a2505d6c676e27bcc6d5c06
annalisape <anna.moz.pe@gmail.com>
2022-12-13 11:34:42 +0100

    working dymanic second level menu to get better

--- src/app/components/side-nav-content/side-nav-content.component.ts
+++ src/app/components/side-nav-content/side-nav-content.component.ts
@@ -1,7 +1,15 @@
-import {Component, Renderer2, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  Renderer2,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  ViewChildren,
+  QueryList,
+} from "@angular/core";
 import { Observable, of } from "rxjs";
 import { Sidenav } from "src/app/models/sidenav";
-import  { SIDENAV } from './sidenav';
+import { SIDENAV } from "./sidenav";
 
 @Component({
   selector: "app-side-nav-content",
@@ -9,32 +17,45 @@ import  { SIDENAV } from './sidenav';
   styleUrls: ["./side-nav-content.component.scss"],
 })
 export class SideNavContentComponent {
+  @ViewChild("firstLevel") firstLevel!: ElementRef;
+  @ViewChildren("secondLevel") secondLevel!: QueryList<ElementRef>;
 
-  @ViewChild('firstLevel') firstLevel!: ElementRef;
-  @ViewChild('secondLevel') secondLevel!: ElementRef;
   sidenav$: Observable<Sidenav[]>;
   sidenavMenu: Sidenav[] = SIDENAV;
 
-  constructor(private renderer:Renderer2) {
+  constructor(private renderer: Renderer2) {
     this.sidenav$ = of(this.sidenavMenu);
   }
 
-  openSecondLevel() {
-      const first = this.firstLevel.nativeElement;
-      const second = this.secondLevel.nativeElement;
-      this.renderer.addClass(first,'menu-translateX-left');
-      this.renderer.removeClass(first,'menu-visible');
-      this.renderer.addClass(second,'menu-translateX');
-      this.renderer.addClass(second,'menu-visible');
-      this.renderer.removeClass(second,'menu-translateX-right');
-  }
-  backFirst() {
+  openSecondLevel(id: string) {
+    console.log(id);
     const first = this.firstLevel.nativeElement;
-    const second = this.secondLevel.nativeElement;
-    this.renderer.removeClass(first,'menu-translateX-left');
-    this.renderer.addClass(first,'menu-visible');
-    this.renderer.removeClass(second,'menu-translateX');
-    this.renderer.removeClass(second,'menu-visible');
-    this.renderer.addClass(second,'menu-translateX-right');
+    // const second = this.secondLevel.nativeElement;
+    const el = document.getElementById("second-" + id);
+    // this.secondLevel.forEach( item => {
+    //   this.renderer
+    //   this.renderer.addClass(item,'menu-translateX');
+    //   this.renderer.addClass(item,'menu-visible');
+    //   this.renderer.removeClass(item,'menu-translateX-right');
+    // })
+    console.log(el);
+    el?.classList.add("menu-translateX");
+    el?.classList.add("menu-visible");
+    el?.classList.remove("menu-translateX-right");
+    this.renderer.addClass(first, "menu-translateX-left");
+  }
+
+  backFirst(id: string) {
+    const first = this.firstLevel.nativeElement;
+    //const second = this.secondLevel.nativeElement;
+    this.renderer.removeClass(first, "menu-translateX-left");
+    this.renderer.addClass(first, "menu-visible");
+    // this.renderer.removeClass(second,'menu-translateX');
+    // this.renderer.removeClass(second,'menu-visible');
+    // this.renderer.addClass(second,'menu-translateX-right');
+    const el = document.getElementById("second-" + id);
+    el?.classList.remove("menu-translateX");
+    el?.classList.remove("menu-visible");
+    el?.classList.add("menu-translateX-right");
   }
 }
