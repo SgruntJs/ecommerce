1fe3533e5498c683f145f09fe6cec88e3e87bff5
annalisape <anna.moz.pe@gmail.com>
2022-12-13 13:42:50 +0100

    wip products-carousel

--- src/app/components/product-carousel/product-carousel.component.ts
+++ src/app/components/product-carousel/product-carousel.component.ts
@@ -37,9 +37,10 @@ export class ProductCarouselComponent implements OnInit {
 
   ngOnInit() {
     this.preloadImages(); // for the demo
+    console.log(this.slides);
   }
 
-  isOpen = true;
+ 
 
   preloadImages() {
     for (const slide of this.slides) {
@@ -51,7 +52,7 @@ export class ProductCarouselComponent implements OnInit {
     const previous = this.currentSlide - 1;
     this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
     console.log("previous clicked, new current slide is: ", this.currentSlide);
-    this.isOpen = !this.isOpen;
+  
     this.direction = '-100';
     this.directionLeave = '100';
   }
@@ -60,7 +61,7 @@ export class ProductCarouselComponent implements OnInit {
     const next = this.currentSlide + 1;
     this.currentSlide = next === this.slides.length ? 0 : next;
     console.log("next clicked, new current slide is: ", this.currentSlide);
-    this.isOpen = this.isOpen;
+ 
     this.direction = '100';
     this.directionLeave = '-100';
   }
