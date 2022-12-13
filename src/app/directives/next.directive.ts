580aba89482f41182534749b28bc72d1cc29341e
annalisape <anna.moz.pe@gmail.com>
2022-12-13 17:30:27 +0100

    wip

--- src/app/directives/next.directive.ts
+++ src/app/directives/next.directive.ts
@@ -1,19 +1,19 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appNext]'
 })
 export class NextDirective {
 
-  constructor(private el: ElementRef) { 
+  constructor(private el: ElementRef, private renderer: Renderer2) { 
     
   }
 
   @HostListener('click')
   nextFunc() {
     const element = this.el.nativeElement.parentElement.children[0];
-    const item = element.getElementsByClassName("item");
-    element.append(item[0]);
+    this.renderer.setStyle(element, 'transform', 'translateX(100%)');
+    this.renderer.setStyle(element, 'transition', 'all .3s ease-in-out');
   }
 
 }
--- src/app/directives/prev.directive.ts
+++ src/app/directives/prev.directive.ts
@@ -1,19 +1,20 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appPrev]'
 })
 export class PrevDirective {
 
-  constructor(private el: ElementRef) { 
+  constructor(private el: ElementRef, private renderer: Renderer2) { 
     
   }
 
   @HostListener('click')
   prevFunc() {
     const element = this.el.nativeElement.parentElement.children[0];
-    const item = element.getElementsByClassName("item");
-    element.prepend(item[item.length - 1]);
+    console.log('element');
+    this.renderer.setStyle(element, 'transform', 'translateX(-100%)');
+    this.renderer.setStyle(element, 'transition', 'all .3s ease-in-out');
   }
 
 }
