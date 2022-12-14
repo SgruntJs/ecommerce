85235d803c8b9e4afa8c8514698464db168174c0
annalisape <anna.moz.pe@gmail.com>
2022-12-14 17:14:01 +0100

    wip custom bounce effect on slider

--- src/app/components/product-carousel/product-carousel.component.ts
+++ src/app/components/product-carousel/product-carousel.component.ts
@@ -1,53 +1,34 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, HostListener, AfterViewInit, ViewChild, ElementRef, Renderer2 } from "@angular/core";
+import { fromEvent, Subscription } from "rxjs";
 import { Slide } from "src/app/models/slide";
-
-import {
-  trigger,
-  transition,
-  style,
-  animate,
-  state,
-} from "@angular/animations";
-
 @Component({
   selector: 'app-product-carousel',
   templateUrl: './product-carousel.component.html',
   styleUrls: ['./product-carousel.component.scss'],
-  animations: [
-    trigger('slideInOut', [
-      state('in', style({ transform: 'translateX(0)' })),
-      transition(':enter', [
-        style({ transform: 'translateX({{direction}}%)' }),
-        animate('300ms ease-in-out', style({ transform: 'translateX(0%)' })),
-      ]),
-      transition(":leave", [
-        animate("300ms ease-in-out", style({ transform: "translateX({{directionLeave}}%)" })),
-      ]),
-      
-    ])
-  ]
 })
-export class ProductCarouselComponent implements OnInit {
+export class ProductCarouselComponent implements OnInit, AfterViewInit {
   @Input() title!: string;
   @Input() slides: Slide[] = [];
-  direction = '0';
-  directionLeave = '0';
-  currentSlide = 0;
 
-  constructor() {}
+  @ViewChild('button') button!: ElementRef;
+  buttonSubscription = new Subscription;
+  buttonSubscription2 = new Subscription;
+
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     this.preloadImages(); // for the demo
-  }
-
-  
-  slideConfig = { slidesToShow:7, slidesToScroll: 6, infinite: false};
-  addSlide() {
     
   }
-  removeSlide() {
-    this.slides.length = this.slides.length - 1;
+
+  ngAfterViewInit() {
+    this.buttonClickPrev();
+    this.buttonClickNext();
   }
+
+  slideConfig = { slidesToShow:7, slidesToScroll: 6, infinite: false};
+ 
+ 
   slickInit(e: any) {
     console.log('slick initialized');
   }
@@ -69,21 +50,44 @@ export class ProductCarouselComponent implements OnInit {
     }
   }
 
-  onPreviousClick() {
-    const previous = this.currentSlide - 1;
-    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
-    console.log("previous clicked, new current slide is: ", this.currentSlide);
-  
-    this.direction = '-100';
-    this.directionLeave = '100';
+  buttonClickPrev() {
+    const btnPrev = this.button.nativeElement.parentNode.parentElement.previousElementSibling;
+    const lastSlide = this.button.nativeElement.parentElement;
+    const btnNext = this.button.nativeElement.parentNode.parentElement.nextElementSibling;
+    
+    console.log(btnNext);
+     
+      this.buttonSubscription = fromEvent(btnPrev, 'click')
+      .subscribe( (res: any) => {
+        console.log('ciao', res.target);
+        if(res.target.classList.contains('slick-disabled')) {
+            console.log('bomba');
+            this.renderer.addClass(lastSlide, 'bounce-prev')
+            setTimeout(() => {
+              lastSlide.classList.remove('bounce-prev');
+            }, 500)
+        }
+      })
+    
+  }
+  buttonClickNext() {
+
+    const lastSlide = this.button.nativeElement.parentElement;
+    const btnNext = this.button.nativeElement.parentNode.parentElement.nextElementSibling;
+    console.log('next', lastSlide)
+    this.buttonSubscription2 = fromEvent(btnNext, 'click')
+      .subscribe( (res: any) => {
+        console.log('ciao', res.target);
+        if(res.target.classList.contains('slick-disabled')) {
+            console.log('bomba');
+            this.renderer.addClass(lastSlide, 'bounce-next')
+            setTimeout(() => {
+              lastSlide.classList.remove('bounce-next');
+            }, 500)
+        }
+      })
+    
   }
 
-  onNextClick() {
-    const next = this.currentSlide + 1;
-    this.currentSlide = next === this.slides.length ? 0 : next;
-    console.log("next clicked, new current slide is: ", this.currentSlide);
  
-    this.direction = '100';
-    this.directionLeave = '-100';
-  }
 }
