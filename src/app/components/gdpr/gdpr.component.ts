fe6bba7db58fed2e3ccc1217057c01dfea4f0636
annalisape <anna.moz.pe@gmail.com>
2022-12-23 18:09:06 +0100

    add close function for gdpr container on click

--- src/app/components/gdpr/gdpr.component.ts
+++ src/app/components/gdpr/gdpr.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-gdpr',
   templateUrl: './gdpr.component.html',
   styleUrls: ['./gdpr.component.scss']
 })
-export class GdprComponent {
+export class GdprComponent implements AfterViewInit, OnDestroy {
+
+  hide = false;
+
+  @ViewChild('btnContinue') btnContinue!: ElementRef;
+  clickedElement: Subscription = new Subscription();
+
+  ngAfterViewInit() {
+      this.toggleHide();
+  }
+
+  toggleHide() {
+    this.clickedElement = fromEvent(this.btnContinue.nativeElement, 'click').subscribe(() => {
+          this.hide = !this.hide;
+    });
+  }
+
+  ngOnDestroy() {
+    this.clickedElement.unsubscribe();
+  }
+
 
 }
