f1af82ab22467eac37003f361141bb1c435ea499
annalisape <anna.moz.pe@gmail.com>
2023-01-02 14:17:31 +0100

    add searchbar on movìbile version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -64,7 +65,8 @@ import { GdprComponent } from './components/gdpr/gdpr.component';
     SlickCarouselModule,
     MatDialogModule,
     MatBottomSheetModule,
-    MatRadioModule
+    MatRadioModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
