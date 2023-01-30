6272ee28d24c59c8c8044a52ea19722fcb076ffc
annalisape <anna.moz.pe@gmail.com>
2023-01-30 10:59:26 +0100

    update model and product template

--- src/app/components/products/product.ts
+++ src/app/components/products/product.ts
@@ -2,9 +2,10 @@ export interface Product {
   id: number;
   name: string;
   price: number;
-  sponsor: true;
+  sponsor: boolean;
   rating: number;
   description: string;
   thumbImage: string;
   category: [];
+  amazonChoice: boolean
 }
